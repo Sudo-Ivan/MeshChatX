6ea5fdfe2bf394c10568dc1afc7ab89219fbcc96
Sudo-Ivan <ivan@quad4.io>
2026-03-06 01:24:52 -0600

    Update package version for reticulum-meshchatx to 4.2.1 in flake.nix

--- flake.nix
+++ flake.nix
@@ -74,7 +74,7 @@
         # Simple package definition for the backend
         packages.default = pkgs.python312Packages.buildPythonPackage {
           pname = "reticulum-meshchatx";
-          version = "4.2.0";
+          version = "4.2.1";
           src = ./.;
           format = "pyproject";
 
