d26f2e40d49e8ce1f01d4f1db4a54c2c6fb91964
Ivan <ivan@quad4.io>
2026-04-08 16:17:00 -0500

    feat(meshchat): add utility modules for environment variable parsing, file path resolution, and self-signed SSL certificate generation

---
+++ meshchatx/src/env_utils.py
@@ -0,0 +1,10 @@
+"""Environment variable parsing helpers."""
+
+import os
+
+
+def env_bool(env_name, default=False):
+    val = os.environ.get(env_name)
+    if val is None:
+        return default
+    return val.lower() in ("true", "1", "yes", "on")
