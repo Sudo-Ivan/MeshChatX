2571fad14c05c9a32f3b7af8c44cf4c79a001901
Ivan <ivan@quad4.io>
2026-04-19 15:59:39 -0500

    chore(ci): update POETRY_VERSION to 2.3.4 in GitHub CI script

--- scripts/ci/github-install-poetry.sh
+++ scripts/ci/github-install-poetry.sh
@@ -3,7 +3,7 @@
 # Set POETRY_VERSION to override the default.
 set -euo pipefail
 
-POETRY_VERSION="${POETRY_VERSION:-2.1.1}"
+POETRY_VERSION="${POETRY_VERSION:-2.3.4}"
 
 python -m pip install --disable-pip-version-check --upgrade pip
 python -m pip install --disable-pip-version-check "poetry==${POETRY_VERSION}"
